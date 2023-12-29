a34b145fcf170eab7e2ef454d632864994ca5e44
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2023-12-29 16:30:37 +0100

    Update explanation of the dirty fix

--- lib/Dialect/lir/LirDialect.cpp
+++ lib/Dialect/lir/LirDialect.cpp
@@ -11,7 +11,7 @@
 /* Dirty fix: Lir.h includes the declaration of mlir::lir::detail::NoisyQubitTypeStorage
  * However, since it is a declaration, it cannot declare any inheritance (like this definition)
  * This leads to a compile-time assert failure, where the NoisyQubitTypeStorage struct cannot be
- * instantiated by the MLIR templaces, since it is not a complete definition:
+ * instantiated by the MLIR templates, since it is not a complete definition:
  * /usr/bin/../lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11/type_traits:1339:23: error: incomplete type 'mlir::lir::detail::NoisyQubitTypeStorage' used in type trait expression
  * 1339 |                     __bool_constant<__has_trivial_destructor(_Tp)>>
  *      |                                     ^
@@ -38,13 +38,19 @@
  *      |     ^
  * /home/diego/code/qoala-mlir-b/build/include/Dialect/lir/LirTypes.h.inc:40:8: note: forward declaration of 'mlir::lir::detail::NoisyQubitTypeStorage'
  *   40 | struct NoisyQubitTypeStorage;
- * The correct declaration (and definition) of the struct is available in the "Dialect.lir/Dialect.h.inc" file
+ * The correct declaration (and definition) of the struct is available in the "Dialect/lir/Dialect.h.inc" file
  * (provided that we use the #define GET_TYPEDEF_CLASSES, just like in "Lir.h"). This makes this file compile, however
  * *it makes fail the linking of `lir-opt`*, since the class defintion is found twice in the linked libraries (once here,
- * and once in the libMLIRLir.a, since Lir.cpp uses the .h.inc with the  #define GET_TYPEDEF_CLASSES macro).
- * SOLUTION: insert a "shell" declaration here, so the instantiation of the templates work correctly. When linking
- * (and knowing that the C linker is super simple), the "expanded" definition fo this class (which contains methods and stuff)
- * will link with no problems, since the dispacther will find the right mangled method name within the execution binaries.
+ * and once in the libMLIRLir.a, since Lir.cpp also uses the"Dialect/lir/Dialect.h.inc with the #define GET_TYPEDEF_CLASSES macro).
+ * BARCKGROUND: Since C and C++ share the linker, a struct/class definition (struct/class name that contains methods) need to be
+ * translated by the compiler into something that the linker can understand. To this end, the compiler will simply "extract" the
+ * methods and translated their named into the mangled ones, and export those sumbols in the binary. After this happens (at compile
+ * time), a C++ class or struct is not different from a C struct or union. The mangled names only become meaningful at runtime,
+ * since the dispatcher needs to know which method to invoke; for this, it uses the mangled name of the method.
+ * SOLUTION: Knowing this we can simply insert a "shell" *declaration* of the struct here (declaring the inheritance), so the
+ * instantiation of the templates work correctly. When linking (and knowing that the C/C++ linker will look for the mangled names),
+ * the "expanded" definition for this class (which contains methods and stuff) will link with no problems, since the linker will
+ * simply find the right mangled method name within the linked libraries/objects.
  */
 
 namespace mlir::lir::detail {
