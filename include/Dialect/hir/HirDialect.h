664042e70a3e01b1688ff440f278d50eaf0a7123
Bart van der Vecht <b.vandervecht@tudelft.nl>
2024-02-23 16:56:47 +0100

    Add initial HIR dialect + std dialect tests

---
+++ include/Dialect/hir/HirDialect.h
@@ -0,0 +1,6 @@
+#ifndef HIR_DIALECT_H
+#define HIR_DIALECT_H
+
+#include "Dialect/hir/HirDialect.h.inc"
+
+#endif // HIR_DIALECT_H
---
+++ lib/Dialect/hir/HirDialect.cpp
@@ -0,0 +1,30 @@
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/Types.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "Dialect/hir/HirDialect.h"
+#include "Dialect/hir/Hir.h"
+
+// important! otherwise the source code in this inc file is not linked into the lib
+#include "Dialect/hir/HirDialect.cpp.inc"
+
+using namespace mlir;
+using namespace mlir::hir;
+
+void HirDialect::initialize()
+{
+    // addOperations<ConstantOp>();
+
+    addOperations<
+#define GET_OP_LIST
+#include "Dialect/hir/Hir.cpp.inc"
+        >();
+
+    addTypes<
+#define GET_TYPEDEF_LIST
+#include "Dialect/hir/HirTypes.cpp.inc"
+        >();
+}
