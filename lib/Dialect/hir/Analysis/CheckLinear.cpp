532941ad0eb14ac060475aa9b50c5fd2b9a5b454
Bart van der Vecht <b.vandervecht@tudelft.nl>
2024-03-01 14:35:03 +0100

    Improve error messages and tests for invalid code

--- lib/Dialect/hir/Analysis/CheckLinear.cpp
+++ lib/Dialect/hir/Analysis/CheckLinear.cpp
@@ -2,7 +2,7 @@
 #include "Dialect/hir/Passes.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Diagnostics.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
 
 namespace mlir {
 #define GEN_PASS_DEF_HIRCHECKLINEAR
@@ -40,33 +40,35 @@ bool isQuantumOp(mlir::Operation *op) {
 }
 
 void HirCheckLinearPass::runOnOperation() {
-  // llvm::outs() << "\n=== Start of CheckLinear pass ===\n\n";
+  // llvm::dbgs() << "\n=== Start of CheckLinear pass ===\n\n";
 
   Operation *operation = getOperation();
 
   if (ModuleOp module = llvm::dyn_cast<ModuleOp>(operation)) {
     // Walk over all ops in the module.
-    module.walk([](Operation *op) {
+    module.walk([this](Operation *op) {
       if (isQuantumOp(op)) {
         // For all ops that produce quantum values, check the results.
         for (auto result : op->getResults()) {
           // Check if the result value has zero or one use.
           if (!result.hasOneUse() && !result.use_empty()) {
             // More than 1 use.
-            op->emitError("Result of operation is used more than once.\n");
             for (auto &use : result.getUses()) {
               auto usingOp = use.getOwner();
-              emitError(usingOp->getLoc(), "Used here\n");
+              usingOp->emitError("quantum operation is used more than once\n");
             }
+            signalPassFailure();
           }
         }
       }
     });
   } else {
-    llvm::errs() << "Not a ModuleOp: something went wrong.\n";
+    operation->emitError("Not a ModuleOp: something went wrong.");
+    signalPassFailure();
+    return;
   }
 
-  // llvm::outs() << "\n=== End of CheckLinear pass ===\n\n";
+  // llvm::dbgs() << "\n=== End of CheckLinear pass ===\n\n";
 }
 
 std::unique_ptr<Pass> mlir::createHirCheckLinearPass() {
--- test/Dialect/hir/Analysis/linear_check_invalid.mlir
+++ test/Dialect/hir/Analysis/linear_check_invalid.mlir
@@ -1,11 +1,9 @@
-// RUN: hir-opt --hir-check-linear %s 2>&1 | FileCheck %s
-
-// CHECK: error: Result of operation is used more than once.
+// RUN: hir-opt --hir-check-linear %s --verify-diagnostics
 
 module {
     %q0 = "hir.new_qubit"() : () -> !hir.qubit
-    %q1 = "hir.hadamard"(%q0) : (!hir.qubit) -> !hir.qubit
-    %q2 = "hir.hadamard"(%q0) : (!hir.qubit) -> !hir.qubit
+    %q1 = "hir.hadamard"(%q0) : (!hir.qubit) -> !hir.qubit  // expected-error {{quantum operation is used more than once}}
+    %q2 = "hir.hadamard"(%q0) : (!hir.qubit) -> !hir.qubit  // expected-error {{quantum operation is used more than once}}
     %q3 = "hir.hadamard"(%q1) : (!hir.qubit) -> !hir.qubit
-    %q4 = "hir.hadamard"(%q1) : (!hir.qubit) -> !hir.qubit
+    %q4 = "hir.hadamard"(%q0) : (!hir.qubit) -> !hir.qubit  // expected-error {{quantum operation is used more than once}}
 }
