942b260c0bf329d516dfa130709ec2d0dec653fc
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-03-15 10:20:48 +0100

    Add helper class to print the Operations being analyzed by the plugin.

--- lib/lowering/HirToLir.cpp
+++ lib/lowering/HirToLir.cpp
@@ -1,6 +1,9 @@
-#include "lowering/HIRToLIR.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Diagnostics.h"
+#include "llvm/Support/Debug.h"
+
+#include "lowering/HIRToLIR.h"
+#include "lowering/Helpers.h"
 
 namespace mlir {
 #define GEN_PASS_DEF_HIRTOLIR
@@ -8,15 +11,19 @@ namespace mlir {
 }
 
 using namespace mlir;
+using namespace llvm;
+using namespace qoala::helpers;
 
-class HIRtoLIRPass : public impl::HIRtoLIRBase<HIRtoLIRPass> {
+class HIRtoLIRPass : public mlir::impl::HIRtoLIRBase<HIRtoLIRPass> {
 public:
     void runOnOperation() override;
+
 };
 
 void HIRtoLIRPass::runOnOperation()  {
-    // TODO
-    ModuleOp module = mlir::OperationPass<ModuleOp>::getOperation();
+    Operation *op = getOperation();
+    qoala::helpers::resetIndent();
+    qoala::helpers::printOperation(op);
 }
 
 std::unique_ptr<mlir::Pass> mlir::createHirToLirPass() {
