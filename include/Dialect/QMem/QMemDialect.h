1a9e38a91ef0fdc693e1d866100f20ed28373c14
Bart van der Vecht <b.vandervecht@tudelft.nl>
2024-03-26 10:39:45 +0100

    Initial implementation

---
+++ include/Dialect/QMem/QMemDialect.h
@@ -0,0 +1,6 @@
+#ifndef QMEM_DIALECT_H
+#define QMEM_DIALECT_H
+
+#include "Dialect/QMem/QMemDialect.h.inc"
+
+#endif // QMEM_DIALECT_H
---
+++ lib/Dialect/QMem/QMemDialect.cpp
@@ -0,0 +1,30 @@
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/Types.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "Dialect/QMem/QMem.h"
+#include "Dialect/QMem/QMemDialect.h"
+
+// important! otherwise the source code in this inc file is not linked into the
+// lib
+#include "Dialect/QMem/QMemDialect.cpp.inc"
+
+using namespace mlir;
+using namespace mlir::qmem;
+
+void QMemDialect::initialize() {
+  // addOperations<ConstantOp>();
+
+  addOperations<
+#define GET_OP_LIST
+#include "Dialect/QMem/QMem.cpp.inc"
+      >();
+
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "Dialect/QMem/QMemTypes.cpp.inc"
+      >();
+}
