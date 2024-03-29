030fd2e50aa15dec249e051da9d1c368d60ff5cf
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-03-29 17:17:36 +0100

    Correctly allow "remote" op to define a symbol

--- lib/Dialect/QNet/QNetDialect.cpp
+++ lib/Dialect/QNet/QNetDialect.cpp
@@ -1,15 +1,7 @@
 #include "mlir/IR/DialectImplementation.h"
-#include "mlir/IR/Types.h"
-#include "mlir/Support/LLVM.h"
-#include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/InliningUtils.h"
-#include "llvm/ADT/TypeSwitch.h"
 
 #include "Dialect/QNet/QNet.h"
 #include "Dialect/QNet/QNetDialect.h"
-
-#include <execinfo.h>
-#include <iostream>
 
 // important! otherwise the source code in this inc file is not linked into the
 // lib
