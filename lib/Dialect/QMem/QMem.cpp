c2eeefb9c917d741ce4d02b316b13f2706e87908
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-04-08 11:13:08 +0200

    Add "func" and "return" operations to the QMem dialect

--- lib/Dialect/QMem/QMem.cpp
+++ lib/Dialect/QMem/QMem.cpp
@@ -1,6 +1,9 @@
 #include "Dialect/QMem/QMem.h"
 #include "Dialect/QMem/QMemDialect.h"
-#include "Dialect/QMem/QMemTypes.h.inc"
+#include "mlir/Interfaces/FunctionImplementation.h"
+
+using namespace mlir;
+using namespace qoala::dialects::qmem;
 
 // include generated source code for operations
 #define GET_OP_CLASSES
@@ -9,3 +12,23 @@
 // include generated source code for types
 #define GET_TYPEDEF_CLASSES
 #include "Dialect/QMem/QMemTypes.cpp.inc"
+
+
+/* Parse and print functions "ported" from func.func: parse and print */
+ParseResult FuncOp::parse(OpAsmParser &parser, OperationState &result) {
+    auto buildFuncType =
+            [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
+               function_interface_impl::VariadicFlag,
+               std::string &) { return builder.getFunctionType(argTypes, results); };
+
+    return function_interface_impl::parseFunctionOp(
+            parser, result, /*allowVariadic=*/false,
+            getFunctionTypeAttrName(result.name), buildFuncType,
+            getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+void FuncOp::print(OpAsmPrinter &p) {
+    function_interface_impl::printFunctionOp(
+            p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+            getArgAttrsAttrName(), getResAttrsAttrName());
+}
