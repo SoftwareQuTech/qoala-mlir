d82c1f93d971b7b38c5a436dc79783bf2d7016d3
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-04-23 17:22:33 +0200

    Add "local_routine" and "request_routine" operations in NetQASM dialect

--- lib/Dialect/NetQASM/NetQASM.cpp
+++ lib/Dialect/NetQASM/NetQASM.cpp
@@ -16,3 +16,41 @@ using namespace qoala::dialects::netqasm;
 // include generated source code for operations
 #define GET_OP_CLASSES
 #include "Dialect/NetQASM/NetQASM.cpp.inc"
+
+/* Parse and print functions "ported" from func.func: parse and print */
+ParseResult LocalRoutineOp::parse(OpAsmParser &parser, OperationState &result) {
+    auto buildFuncType =
+            [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
+               function_interface_impl::VariadicFlag,
+               std::string &) { return builder.getFunctionType(argTypes, results); };
+
+    return function_interface_impl::parseFunctionOp(
+            parser, result, /*allowVariadic=*/false,
+            getFunctionTypeAttrName(result.name), buildFuncType,
+            getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+void LocalRoutineOp::print(OpAsmPrinter &p) {
+    function_interface_impl::printFunctionOp(
+            p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+            getArgAttrsAttrName(), getResAttrsAttrName());
+}
+
+/* Parse and print functions "ported" from func.func: parse and print */
+ParseResult RequestRoutineOp::parse(OpAsmParser &parser, OperationState &result) {
+    auto buildFuncType =
+            [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
+               function_interface_impl::VariadicFlag,
+               std::string &) { return builder.getFunctionType(argTypes, results); };
+
+    return function_interface_impl::parseFunctionOp(
+            parser, result, /*allowVariadic=*/false,
+            getFunctionTypeAttrName(result.name), buildFuncType,
+            getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+void RequestRoutineOp::print(OpAsmPrinter &p) {
+    function_interface_impl::printFunctionOp(
+            p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+            getArgAttrsAttrName(), getResAttrsAttrName());
+}
