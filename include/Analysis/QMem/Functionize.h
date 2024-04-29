573482e739d4481e26f5401c5ecc1bad88a79a37
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-04-29 15:42:47 +0200

    Refactor functionized code to make it reusable from outside the "functionize" pass (useful wo write the wrapper pass)

---
+++ include/Analysis/QMem/Functionize.h
@@ -0,0 +1,56 @@
+#ifndef QOALA_MLIR_FUNCTIONIZE_H
+#define QOALA_MLIR_FUNCTIONIZE_H
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "Dialect/QMem/QMem.h"
+
+#include <set>
+
+namespace qoala::analysis {
+    struct ArgTypesAndReturns {
+        std::vector<mlir::Type> argTypes;
+        std::vector<mlir::Type> resultTypes;
+        mlir::DenseMap<mlir::Operation *, std::set<uint32_t>> opResultsIndexes;
+    };
+
+    /**
+     * Creates a "wrapper" function around the given list of operations.
+     * WARNING: This function will not perform any type of Data nor Control Flow
+     * analysis on the given operations, and it assumes that the data and control
+     * flow of the given list is consistent.
+     * @param opBuilder An `OpBuilder` object used to clone the given operations.
+     * @param funcName The name of the function to be created.
+     * @param loc The location to use as an attribute for the new operations created.
+     * @param quantumOps An `ArrayRef` object containing the set of operations to wrap.
+     * @return A `func::FuncOp` object, inserted at the insertion point of the given `OpBuilder`,
+     *         whose body contains the given operations, and an extra return statement.
+     */
+    mlir::func::FuncOp createNewFunctionWithOperations(
+            mlir::OpBuilder *opBuilder, mlir::StringRef funcName,
+            mlir::Location loc , llvm::SetVector<mlir::Operation *> & quantumOps);
+
+    /**
+     * Analyzes the given set of operations to determine the "arguments" and "results" of the given set.
+     * To this end the given operations will be treated as a "closure", meaning that all the data coming
+     * from outside the set of operations will be considered an "argument". Similarly, any data that
+     * is used outside the given set, will be considered as a result
+     * @param operations The ser of operations to analyze.
+     * @return An `ArgTypesAndReturn` struct, containing the argument types, result types, and a map
+     *         between an operation (within the given set) that produces a result, and the set of indexes
+     *         of that operation's result that are considered a result of the given operations set.
+     */
+    ArgTypesAndReturns computeArgTypesAndReturns(llvm::SetVector<mlir::Operation *> &operations);
+
+    /**
+     * "Functionizes" the operations on the given module. For each one of the operations
+     * on the module, the given callback will be executed to determine if the involved
+     * operation can be functionized or not.
+     * @param module The module to analyze
+     * @param opCanBeFunctionized A function that takes an operation, and determines if it
+     *                            can be functionized or not.
+     */
+    void functionizeModule(mlir::ModuleOp &module, bool (*opCanBeFunctionized) (mlir::Operation *));
+}
+
+#endif //QOALA_MLIR_FUNCTIONIZE_H
---
+++ lib/Analysis/QMem/Functionize.cpp
@@ -0,0 +1,164 @@
+#include "Analysis/QMem/Functionize.h"
+#include "llvm/Support/Debug.h"
+
+/* MLIR magic: using the LLVM_DEBUG macro + this define, allows to selectively enable the debug output
+ * To enable the prints, simply invoke `opt` with `--debug-only=functionize-poc`, or any other string defined
+ * as the "DEBUG_TYPE" macro */
+#define DEBUG_TYPE "functionize-poc"
+
+using namespace mlir;
+using namespace qoala::analysis;
+
+ArgTypesAndReturns qoala::analysis::computeArgTypesAndReturns(SetVector<Operation *> &quantumOps) {
+    // We need to compute a sort of "closure" in the quantumOps set.
+    // Create a new function at the top level, using the types of the "orphan" usages of the given ops:
+    // If an operation that appears on the "uses" is not in the quantumOps set, then it's an
+    // "outsider", hence it's an argument of the quantumOps set.
+    // If an operation that appears on the "operands" is not in the quantumOps set, then it's an
+    // "outsider", hence it's a result of the quantumOps set.
+    std::vector<Type> argTypes;
+    std::vector<Type> resultTypes;
+
+    // Additionally, the treatment with the results it's a bit complicated
+    // Since we will clone the quantumOp, we need to save the _index_ of the result that is a
+    // return. So later, when we clone the operation we construct the list of the actual result
+    // values using the indexes per operation.
+    mlir::DenseMap<Operation *, std::set<uint32_t>> opResultsIndexes;
+    for (Operation *quantumOp : quantumOps) {
+        auto opOperands = quantumOp->getOperands();
+        for (Value opOperand: opOperands) {
+            if (!quantumOps.contains(opOperand.getDefiningOp())) {
+                argTypes.push_back(opOperand.getType());
+            }
+        }
+        ResultRange opResults = quantumOp->getResults();
+        for (OpResult opResult : opResults) {
+            // A set to store all the indexes of the results that will be returned
+            std::set<uint32_t> returnIndexesForOp;
+            unsigned int resultIndex = opResult.getResultNumber();
+
+            if (opResult.getUses().empty()) {
+                // If there are no uses, we still need to return the "unused" result
+                resultTypes.push_back(opResult.getType());
+                returnIndexesForOp.insert(resultIndex);
+            } else {
+                // If any of the result usages is not in the quantumOps (and that index is not on
+                // the already marked for return), then it needs to be returned
+                for (auto &usage : opResult.getUses()) {
+                    if (!quantumOps.contains(usage.getOwner()) && !returnIndexesForOp.contains(resultIndex)) {
+                        resultTypes.push_back(opResult.getType());
+                        returnIndexesForOp.insert(resultIndex);
+                    }
+                }
+            }
+            opResultsIndexes.insert(std::pair{quantumOp, returnIndexesForOp});
+        }
+    }
+    return ArgTypesAndReturns{argTypes, resultTypes, opResultsIndexes};
+}
+
+func::FuncOp qoala::analysis::createNewFunctionWithOperations(
+        OpBuilder *opBuilder, StringRef funcName,
+        Location loc, SetVector<Operation *> &quantumOps) {
+    ArgTypesAndReturns typesAndValues = computeArgTypesAndReturns(quantumOps);
+
+    // Create the new function, and attach a block to it
+    FunctionType funType = opBuilder->getFunctionType(typesAndValues.argTypes, typesAndValues.resultTypes);
+    auto newFunc = opBuilder->create<func::FuncOp>(loc, funcName, funType);
+    Block *newBlock = newFunc.addEntryBlock();
+
+    LLVM_DEBUG(llvm::dbgs() << "************************\n");
+    LLVM_DEBUG(llvm::dbgs() << "func type: " << funType << "\n");
+
+    // Create a new operation of the same type that the original one, that uses
+    // the function arguments instead of dialect results of other ops
+    // Helper - cast the quantum operation to the "SimpleCloneInterface" type, so
+    // it is possible to invoke "simpleClone" on that operation
+    {
+        LLVM_DEBUG(llvm::dbgs() << "------------------------\n");
+        OpBuilder::InsertionGuard g(*opBuilder);
+        std::vector<Value> resultValues;
+        for (Operation *quantumOp: quantumOps) {
+            LLVM_DEBUG(llvm::dbgs() << "original op: " << *quantumOp << "\n");
+            auto castedOldOp = dyn_cast<dialects::qmem::iface::SimpleCloneInterface>(quantumOp);
+            assert(castedOldOp); // We expect that the cast will succeed
+
+            // New operations (including the clone) should be attached to the block of the new function
+            opBuilder->setInsertionPointToStart(newBlock);
+            auto clonedOp = castedOldOp.simpleClone(*opBuilder, newFunc.getLoc());
+            clonedOp->setOperands(newBlock->getArguments());
+
+            // Populate the results from the current operation, matching the indexes already computed
+            for (uint32_t resultIndex: typesAndValues.opResultsIndexes[quantumOp]) {
+                resultValues.push_back(clonedOp->getResult(resultIndex));
+            }
+            LLVM_DEBUG(llvm::dbgs() << "cloned op: " << *clonedOp << "\n");
+        }
+        LLVM_DEBUG(llvm::dbgs() << "------------------------\n");
+
+        // Create the "return" for the new operation
+        auto returnOp = opBuilder->create<func::ReturnOp>(newFunc.getLoc(), resultValues);
+        LLVM_DEBUG(llvm::dbgs() << "New Function: " << newFunc << "\n");
+        LLVM_DEBUG(llvm::dbgs() << "************************\n");
+    }
+    return newFunc;
+}
+
+// Identifier to make the names of the new function definitions unique
+static int identifier = 0;
+
+void qoala::analysis::functionizeModule(ModuleOp &module, bool (*opCanBeFunctionized) (Operation *)) {
+    SmallVector<Operation *, 100> quantumOps;
+    DenseMap<Operation *, func::FuncOp> mappedFunctions;
+
+    // Discover and mark the quantum operations
+    module.walk([&](Operation *operation) {
+        if (opCanBeFunctionized(operation)) {
+            quantumOps.push_back(operation);
+        }
+    });
+
+    // We start building our new functions at the start of the body of the module
+    OpBuilder opBuilder(module.getBodyRegion());
+    Location topLocation = module.getBodyRegion().front().getOperations().front().getLoc();
+
+    for (Operation *quantumOp : quantumOps) {
+        // std::format was introduced as part of C++20 standard. This is included since GCC 13,
+        // which is NOT part of the standard GCC package of ubuntu 22 (bundles GCC 11)
+        // To avoid depending on headers included on a compiler that has to be manually installed,
+        // or an extra library (like LLVM's libc++), we will default to good'ol 70's C way of
+        // formatting a string
+        // #include <format>
+        //std::string funcName = std::format("__qoala_wrapper{}", identifier++);
+        // Format the name of the new function
+        auto nameFormat = "__qoala_wrapper%d";
+        int length = std::snprintf(nullptr, 0, nameFormat, identifier);
+        std::vector<char> funcName(length + 1);
+        std::sprintf(funcName.data(), nameFormat, identifier++);
+
+        // We create a new function that will contain the single operation in its body (+ a return statement)
+        SetVector<Operation *> operations;
+        operations.insert(quantumOp);
+        func::FuncOp newFunc = createNewFunctionWithOperations(
+                &opBuilder, std::string(funcName.data()), topLocation, operations
+        );
+        auto insertedSymbol = module.lookupSymbol<func::FuncOp>(newFunc.getNameAttr());
+        mappedFunctions.insert(std::pair{quantumOp, insertedSymbol});
+    }
+    for (Operation *quantumOp : quantumOps) {
+        // Create a "call" operation in place of the original operation
+        OpBuilder::InsertionGuard g(opBuilder);
+        opBuilder.setInsertionPointAfter(quantumOp);
+        auto callOp = opBuilder.create<func::CallOp>(
+                quantumOp->getLoc(), mappedFunctions[quantumOp],
+                /*args=*/ValueRange(quantumOp->getOperands())
+        );
+
+        // Finally, replace the usages of the old operation with the result of the "call"
+        // and delete the old operation
+        LLVM_DEBUG(llvm::dbgs() << "Replacing: " << *quantumOp << "\n");
+        LLVM_DEBUG(llvm::dbgs() << "With: " << callOp << "\n");
+        quantumOp->replaceAllUsesWith(callOp);
+        quantumOp->erase();
+    }
+}
