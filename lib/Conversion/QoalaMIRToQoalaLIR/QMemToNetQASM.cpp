30920dc8887e0caac0da266d1f51e30124535832
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-04-29 14:22:07 +0200

    Add skeleton bodies to lowering QMem to QoalaHost and NetQASM dialects

--- lib/Conversion/QoalaMIRToQoalaLIR/QMemToNetQASM.cpp
+++ lib/Conversion/QoalaMIRToQoalaLIR/QMemToNetQASM.cpp
@@ -14,13 +14,43 @@ namespace mlir {
 
 #define DEBUG_TYPE "qmem-to-netqasm"
 
+using namespace mlir;
+using namespace llvm;
+using namespace qoala::helpers;
+using namespace qoala::dialects;
+
 namespace qoala::conversion {
     class LowerQMemToNetQASMPass : public mlir::impl::LowerQMemToNetQASMBase<LowerQMemToNetQASMPass> {
         void runOnOperation() override;
     };
 
     void LowerQMemToNetQASMPass::runOnOperation() {
-        // TODO - To Implement
+        MLIRContext &context = this->getContext();
+        ModuleOp operation = dyn_cast<ModuleOp>(getOperation());
+
+        ConversionTarget target(context);
+        target.addLegalDialect<netqasm::NetQASMDialect>();
+        target.addIllegalDialect<qmem::QMemDialect>();
+        target.addLegalOp<
+                // Only the "qmem.func" and "qmem.return" operations are legal after this pass,
+                // because this pass modifies ALL the other operations but the main function
+                qmem::FuncOp,
+                qmem::ReturnOp
+        >();
+
+        // We add the conversion pattern to the context
+        RewritePatternSet patterns(&context);
+        // We don't need a type converter in this stage
+        //QoalaMIRToQoalaLIRTypeConverter typeConverter(&context);
+//        patterns.add<
+                // TODO - To Implement
+//        >(&context);
+
+        LogicalResult result =
+                mlir::applyPartialConversion(operation, target, std::move(patterns));
+        if (mlir::failed(result)) {
+            signalPassFailure();
+        }
     }
 }
 
