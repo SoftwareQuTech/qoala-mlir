2f3256311118c434d5f9deafbc0de16a69d4e399
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-05-03 13:13:50 +0200

    Add f32 rotation conversion before applying the final lowering from QMem to NetQASM

--- lib/Conversion/QoalaMIRToQoalaLIR/QMemToNetQASM.cpp
+++ lib/Conversion/QoalaMIRToQoalaLIR/QMemToNetQASM.cpp
@@ -80,12 +80,25 @@ namespace qoala::conversion {
         qoala::helpers::configureQMemToNetQASMTarget(target);
         qoala::helpers::populateQMemToNetQASMPatterns(context, patterns, typeConverter);
 
-        // TODO - Add the "intermediate" lowering stage here
+        // Lowering QMem to NetQASM expect to lower rotations. so we need to lower the
+        // f32 rotations using the intermediate step
+        ConversionTarget f32LoweringTarget(context);
+        RewritePatternSet f32Patterns(&context);
+        qoala::helpers::configureF32LoweringTarget(f32LoweringTarget);
+        qoala::helpers::populateQMemF32ToInt32RotPatterns(context, f32Patterns, typeConverter);
 
         if (!moduleContainsAngleConversionDeclaration(module)) {
             insertAngleConversionFunctionDeclaration(module);
         }
 
+        // First, lower f32 rotations
+        LogicalResult f32LoweringResult =
+                mlir::applyPartialConversion(module, f32LoweringTarget, std::move(f32Patterns));
+        if (mlir::failed(f32LoweringResult)) {
+            signalPassFailure();
+        }
+
+        // Then, lower the rest
         LogicalResult result =
                 mlir::applyPartialConversion(module, target, std::move(patterns));
         if (mlir::failed(result)) {
