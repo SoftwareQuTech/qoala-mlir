a5d63948ef1babcd09e3dec2c0a31bc723e127de
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-05-07 16:53:50 +0200

    Add QRemote generated code in the qremote library (code doesn't compile)

---
+++ lib/Dialect/QRemote/QRemote.cpp
@@ -0,0 +1,13 @@
+#include "Dialect/QRemote/QRemote.h"
+#include "Dialect/QRemote/QRemote.h"
+
+using namespace mlir;
+using namespace qoala::dialects::qremote;
+
+// include generated source code for operations
+#define GET_OP_CLASSES
+#include "Dialect/QRemote/QRemote.cpp.inc"
+
+// include generated source code for types
+#define GET_TYPEDEF_CLASSES
+#include "Dialect/QRemote/QRemoteTypes.cpp.inc"
---
+++ lib/Dialect/QRemote/QRemoteDialect.cpp
@@ -0,0 +1,28 @@
+#include "mlir/IR/DialectImplementation.h"
+//#include "mlir/IR/Types.h"
+//#include "mlir/Support/LLVM.h"
+//#include "mlir/Support/LogicalResult.h"
+//#include "mlir/Transforms/InliningUtils.h"
+//#include "llvm/ADT/TypeSwitch.h"
+
+#include "Dialect/QRemote/QRemote.h"
+#include "Dialect/QRemote/QRemoteDialect.h"
+
+// important! otherwise the source code in this inc file is not linked into the
+// lib
+#include "Dialect/QRemote/QRemoteDialect.cpp.inc"
+
+using namespace mlir;
+using namespace qoala::dialects::qremote;
+
+void QRemoteDialect::initialize() {
+    addOperations<
+#define GET_OP_LIST
+#include "Dialect/QRemote/QRemote.cpp.inc"
+        >();
+
+    addTypes<
+#define GET_TYPEDEF_LIST
+#include "Dialect/QRemote/QRemoteTypes.cpp.inc"
+        >();
+}
