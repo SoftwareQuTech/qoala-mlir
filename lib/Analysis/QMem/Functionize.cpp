54a3f7afcce5bda57ee3a3ee0c0f4b83e69af150
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-05-14 15:25:12 +0200

    Improved first PoC of the grouping for functionize

--- lib/Analysis/QMem/Functionize.cpp
+++ lib/Analysis/QMem/Functionize.cpp
@@ -1,3 +1,4 @@
+#include <llvm/ADT/SmallSet.h>
 #include "Analysis/QMem/Conversion.h"
 #include "Dialect/QMem/EntangleTrait.h"
 #include "llvm/Support/Debug.h"
@@ -307,35 +308,78 @@ namespace qoala::analysis::functionize {
         >(op);
     }
 
-    static bool qMemOpShoudlRemainInBody(Operation &op) {
+    static bool qMemOpIsClassicalCommunication(Operation &op) {
         return llvm::isa<
                 dialects::qmem::RecvFloatsOp,
-                dialects::qmem::RecvIntsOp
+                dialects::qmem::RecvIntsOp,
+                dialects::qmem::SendFloatsOp,
+                dialects::qmem::SendIntsOp
         >(op);
     }
 
-    static bool qMemIsEPRSOperation(Operation &op) {
-        return llvm::isa<dialects::qmem::EprsOp>(op);
+    static bool qMemOpInitsQubit(Operation *op) {
+        return llvm::isa<
+                dialects::qmem::EprsOp,
+                dialects::qmem::InitOp
+        >(op);
     }
 
-    static bool qMemAllocatesQubit(Operation &op) {
-        return llvm::isa<dialects::qmem::QAllocOp>(op);
+    static bool qMemOpShouldRemainInBody(Operation &op) {
+        return llvm::isa<
+                dialects::qmem::FuncOp,
+                dialects::qmem::ReturnOp,
+                dialects::qmem::RemoteOp
+        >(op);
     }
 
     std::vector<QuantumOpsGroupTy> functionizeOpClassifier(dialects::qmem::FuncOp &mainFunction) {
         std::vector<QuantumOpsGroupTy> opsGroups;
         std::map<Value, QuantumOpsGroupTy> perQubitGroup;
         QuantumOpsGroupTy currentOpsGroup;
+        std::set<Operation *> groupedOps;
 
         LLVM_DEBUG(llvm::dbgs() << "%%%%%%%%%%%%%%" << "\n");
         LLVM_DEBUG(llvm::dbgs() << "% CLASSIFIER %" << "\n");
+
+        /* Usual lifecycle of a qubit:
+         * QAlloc -> QInit/EPRS_init -> Gate(s) -> (EPRS_)Measure
+         * In this lifecycle, a qubit "is not usable" until it is (eprs) initialized.
+         * WARNING - It is possible that there are quite some operations between the qalloc and
+         * the init/eprs operation. *This implementation makes that assumption that the qinit/eprs
+         * operation can be moved as close as it can to its respective qalloc operation*. This
+         * assumption allows us to simplify the analysis
+         */
+        auto qAllocOperations = mainFunction.getOps<dialects::qmem::QAllocOp>();
+        for (dialects::qmem::QAllocOp qAllocOp : qAllocOperations) {
+            // Special case: qalloc operation has a single use, and it is only for eprs_measure
+            bool opIsQAllocEPRSMeasurePair = false;
+            if (llvm::hasSingleElement(qAllocOp->getUsers())) {
+                opIsQAllocEPRSMeasurePair = true;
+            }
+
+            // Look for the "init" operation of the current qalloc
+            for (Operation *user : qAllocOp->getUsers()) {
+                if (opIsQAllocEPRSMeasurePair || qMemOpInitsQubit(user)) {
+                    // Found: Group both ops in a single group and mark the operations as grouped
+                    LLVM_DEBUG(llvm::dbgs() << " - op: " << qAllocOp << "\n");
+                    LLVM_DEBUG(llvm::dbgs() << " - op: " << *user << "\n");
+                    LLVM_DEBUG(llvm::dbgs() << " - New Group" << "\n");
+                    opsGroups.push_back({qAllocOp, user});
+                    groupedOps.insert(qAllocOp.getOperation());
+                    groupedOps.insert(user);
+                    break;
+                }
+            }
+        }
+
         for (Operation &op : mainFunction.getOps()) {
-            if (!operationBelongsToQMemDialect(op)) {
+            if (!operationBelongsToQMemDialect(op) || qMemOpShouldRemainInBody(op) || groupedOps.contains(&op)) {
+                // Operation is not QMem or it was already grouped
                 continue;
             }
 
-            if (qMemOpShoudlRemainInBody(op)) {
-                // This function should stay in the main body... commit the current state of the group iff it-s not empty
+            if (qMemOpIsClassicalCommunication(op)) {
+                // This function should stay in the main body... commit the current state of the group iff it's not empty
                 if (!currentOpsGroup.empty()) {
                     opsGroups.push_back(currentOpsGroup);
                     currentOpsGroup.clear();
