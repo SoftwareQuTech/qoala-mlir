0bfe1859cfc201fd8725257ec3c6deb2181a99e5
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-05-21 13:39:03 +0200

    Move declaration of entry point function into the correct namespace

--- lib/Target/ModuleTranslation.cpp
+++ lib/Target/ModuleTranslation.cpp
@@ -1,7 +1,7 @@
 #include "Target/iQoala/ModuleTranslation.h"
 #include "mlir/IR/BuiltinOps.h"
 
-std::unique_ptr<qoala::iqoala::Module> translateModuleToiQoala(
+std::unique_ptr<qoala::iqoala::Module> qoala::translate::translateModuleToiQoala(
         mlir::Operation *module, qoala::iqoala::iQoalaContext &iQoalaContext,
         llvm::StringRef name) {
     // TODO - Entry point for the transformations
