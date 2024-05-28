0836e7a897c62c295dc9c4e9085f4cf148f5ee63
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-05-28 17:06:52 +0200

    Add op codes for NetQASM and QoalaHost instructions

--- include/Target/iQoala/ASM.h
+++ include/Target/iQoala/ASM.h
@@ -9,14 +9,45 @@ using namespace llvm;
 namespace qoala::assembly {
     class NetQASMInstruction : public iQoalaMCInstruction {
     public:
-        void print(raw_ostream &os) const override {};
-        // TODO
+        enum OpCode {
+            OP_UNKNOWN = 0,
+            OP_SET,
+            OP_ADD,
+            OP_SUBTRACT,
+            OP_MULTIPLY,
+            OP_QUOT,
+            OP_REM,
+            OP_JUMP,
+            OP_BEQ,
+            OP_BNE,
+            OP_BGT,
+            OP_BLT,
+            OP_LOAD,
+            OP_STORE
+        };
     };
 
     class QoalaHostInstruction : public iQoalaMCInstruction {
     public:
-        void print(raw_ostream &os) const override {};
-        // TODO
+        enum OpCode {
+            OP_UNKNOWN = 0,
+            OP_ASSIGN,
+            OP_ADD,
+            OP_SUBTRACT,
+            OP_MULTIPLY_CONSTANT,
+            OP_BI_COND_MULTIPLY,
+            OP_JUMP,
+            OP_BEQ,
+            OP_BNE,
+            OP_BGT,
+            OP_BLT,
+            OP_SEND_MSG,
+            OP_RECV_MSG,
+            OP_RUN_ROUTINE,
+            OP_RUN_REQUEST,
+            OP_SUBMIT_ROUTINES,
+            OP_JOIN_ROUTINES
+        };
     };
 }
 
