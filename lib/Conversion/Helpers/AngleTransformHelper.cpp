a29c9237c38e992541bdb2e400908baa248b145c
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2025-02-27 16:58:29 +0100

    Add more documentation in the transformDouble algorithm.

--- lib/Conversion/Helpers/AngleTransformHelper.cpp
+++ lib/Conversion/Helpers/AngleTransformHelper.cpp
@@ -53,17 +53,17 @@ namespace qoala::helpers::angle {
      * a = f(n,e) = \frac{n\times\pi}{2^e}.
      * The implementation follows a similar approach w.r.t the Newton-Raphson algorithm.
      * The given angle can be associated to a contour curve on the 2-dimensional
-     * sheet a = f(n,e). The idea is to find that contour curve, and follow it for
-     * some time (limited number of iterations) until we find the best match for the
-     * given angle value.
+     * sheet a = f(n,e). The idea is to find that contour curve, and follow it discretely
+     * (varying n and e as integers) for some time (i.e. limited number of iterations)
+     * until we find the best match for the given angle value.
      * @param angleRads an angle (type double) expressed in radians
      * @return a vector of two integers (n, e)
      */
     std::vector<uint32_t> transformDouble(const double /*a=*/angleRads) {
         // This algorithm is based on some observations:
         // * The contour curve has a logarithmic shape. In fact, assuming that "a"
-        //   is constant, it is not difficult to compute the contour curve:
-        //   e = f(n) = \frac{1}{ln 2}\times ln(n) + \frac{ln a + ln 1}{ln 2}
+        //   is constant, it is not difficult to compute the contour curve for that a:
+        //   e = f(n) = \frac{1}{ln 2}\times ln(n) + \frac{ln \pi - ln a}{ln 2}
         //   This curve can be visualized as "taking a picture from the z axis"
         // * Since the contour curve is logarithmic, lim_{n -> \infty} f(n) = \infty,
         //   hence it is not possible to iterate indefinitely and "automatically stop"
@@ -73,19 +73,28 @@ namespace qoala::helpers::angle {
         //   "ALMOST_PERFECT_THRESHOLD" value. We use it to test the "fitness" of the
         //   found integer pair to eagerly stop searching. The given integer pair yields
         //   an angle that is so close that we can call it "good enough".
+        // * The search starts on the n axis (e = 0), at ceil(a/3). This limit can be
+        //   computed from the general from of the angle.
+        // * The idea is to follow the contour curve, moving only in one axis until we
+        //   cross the contour curve. We also keep track of the best approximation found
+        //   so far (bestN, bestE, bestThreshold)
+        // * Every time we cross the curve, we test the new approximation, comparing it
+        //   with the best results so far.
 
-        // TODO - Keep documenting this function.
         auto n = static_cast<uint32_t>(std::ceil(angleRads / 3.0));
         uint32_t e = 0, bestN = 0, bestE = 0;
         double bestThreshold = 0.0;
 
+        // If angle is 0, then angleCalculator(0, 0) == 0.0, so we simply return.
         if (angleRads != angleCalculator(n, e)) {
-            // The given angle is not 0.0
+            // The initial guess of n should yield an angle value >= than the given one,
+            // We move over the n axis (lower n) until we cross the contour line i.e. we get
+            // an angle value less than the given one.
             while (angleRads <= angleCalculator(n, e)) {
                 n--;
             }
             // In this point we know that angleCalculator(n, 0) < angleRads < angleCalculator(n+1, 0)
-            // Starting point of thew approximation; these are our best results so far:
+            // these are our best results so far:
             n = n + 1;
             bestN = n;
             bestE = e;
@@ -97,7 +106,7 @@ namespace qoala::helpers::angle {
                 double currentAngle = angleCalculator(n, e);
 
                 if (currentAngle == angleRads || bestThreshold <= ALMOST_PERFECT_THRESHOLD) {
-                    // Jackpot! We found a perfect match
+                    // Jackpot! We found a (almost) perfect match
                     break;
                 }
 
@@ -105,9 +114,11 @@ namespace qoala::helpers::angle {
                     // On even iterations, we move over e, on odd iterations we move over n
                     i % 2 == 0 ? e++ : n++;
                     currentAngle = angleCalculator(n, e);
-                    double distance = std::abs(currentAngle - angleRads);
-                    if (distance < bestThreshold) {
-                        bestThreshold = distance;
+                    // Distance between the current set of values, and the given angle
+                    double threshold = std::abs(currentAngle - angleRads);
+                    if (threshold < bestThreshold) {
+                        // We found a better fit for the approximation
+                        bestThreshold = threshold;
                         bestN = n;
                         bestE = e;
                     }
