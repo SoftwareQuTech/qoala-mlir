31dfbd5ed474bbee94615fdc06eef1fd0866876c
spoukke <sacha.bernheim@hey.com>
2025-03-20 15:48:37 +0100

    add tablegen basic structure for qoala host add dependencies pass

---
+++ include/Dialect/QoalaHost/Passes.h
@@ -0,0 +1,17 @@
+#ifndef QOALAHOST_PASSES_H_
+#define QOALAHOST_PASSES_H_
+
+#include "Dialect/QoalaHost/QoalaHostDialect.h"
+#include "mlir/Pass/Pass.h"
+
+namespace qoala::analysis {
+#define GEN_PASS_DECL
+#include "Dialect/QoalaHost/Passes.h.inc"
+
+/// Generate the code for registering passes.
+#define GEN_PASS_REGISTRATION
+#include "Dialect/QoalaHost/Passes.h.inc"
+
+} // namespace mlir
+
+#endif // QOALAHOST_PASSES_H_
