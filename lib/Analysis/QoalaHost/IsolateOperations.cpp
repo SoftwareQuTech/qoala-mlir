dd5bd0523fea03a3cf328eb396e55e0d643fd51e
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2025-03-25 13:19:37 +0100

    Factorize code for "blockification" of operations

--- lib/Analysis/QoalaHost/IsolateOperations.cpp
+++ lib/Analysis/QoalaHost/IsolateOperations.cpp
@@ -1,7 +1,5 @@
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "Analysis/QoalaHost/Helpers.h"
 #include "Dialect/QoalaHost/QoalaHost.h"
-#include "Dialect/QMem/QMem.h"
 
 #include "llvm/Support/Debug.h"
 
@@ -11,7 +9,7 @@ using namespace mlir;
 using namespace qoala::dialects;
 
 namespace qoala::analysis::isolate {
-    static Operation *getNextOperation(Operation *op) {
+    Operation *getNextOperation(Operation *op) {
         Block::iterator it(op);
         ++it;
         if (it != op->getBlock()->end()) {
@@ -20,7 +18,7 @@ namespace qoala::analysis::isolate {
         return nullptr;
     }
 
-    static void isolateOp(Operation *opToIsolate, ConversionPatternRewriter &rewriter) {
+    void isolateOp(Operation *opToIsolate, ConversionPatternRewriter &rewriter) {
         LLVM_DEBUG(llvm::dbgs() << "Isolating operation " << *opToIsolate << "\n");
         Block *originalBlock = opToIsolate->getBlock();
         Operation *opToIsolateInNewBlock;
@@ -37,9 +35,11 @@ namespace qoala::analysis::isolate {
             // We split the block up to the given op (will be the first op of the returned block)
             middleBlock = originalBlock->splitBlock(opToIsolate);
 
-            // Insert a new NopTOp at the end of the original block
-            rewriter.setInsertionPoint(originalBlock, originalBlock->end());
-            rewriter.create<qoalahost::NopTOp>(opToIsolate->getLoc());
+            // Insert a new NopTOp at the end of the original block if needed
+            if (!originalBlock->mightHaveTerminator()) {
+                rewriter.setInsertionPoint(originalBlock, originalBlock->end());
+                rewriter.create<qoalahost::NopTOp>(opToIsolate->getLoc());
+            }
 
             opToIsolateInNewBlock = &middleBlock->front();
             assert(opToIsolate == opToIsolateInNewBlock);
@@ -51,41 +51,16 @@ namespace qoala::analysis::isolate {
         // Split the new block up to the next op to isolate.
         // In this way "nextToOpToIsolate" will be the first op of a new block, and
         // opToIsolate will be effectively isolated in a block (middleBlock)
-        middleBlock->splitBlock(nextToOpToIsolate);
-    }
+        middleBlock->splitBlock(nextToOpToIsolate); /*=bottomBlock*/
 
-    void isolateOpsInNewBlocks(Operation *funcOp, ConversionPatternRewriter &rewriter) {
-        LLVM_DEBUG(llvm::dbgs() << "operation " << *funcOp << "\n");
-        auto mainFunction = dyn_cast<qoalahost::MainFuncOp>(funcOp);
-        assert (mainFunction && "Trying to isolate the operations on an operation that is not a qoalahost.main_func");
-        SmallVector<Operation *> opsToIsolate;
-
-        // To correctly isolate the op, we will simply "mark them", since modifying
-        // the structure of the blocks will invalidate the internal iterators
-        // of the walk functions.
-        mainFunction.walk([&](qmem::RecvIntsOp op) {
-            opsToIsolate.push_back(op.getOperation());
-        });
-
-        mainFunction.walk([&](qmem::RecvFloatsOp op) {
-            opsToIsolate.push_back(op.getOperation());
-        });
-
-        mainFunction.walk([&](func::CallOp op) {
-            // We will only isolate the "func.call" operations that have the
-            // "functionaized" attribute, since they were created by the functionization
-            // algorithm, hence, they will be mapped to "qoalahost.call" operation, calling
-            // either a "netqasm.local_routine" or a "netqasm.request_routine" operations.
-            if (Attribute attr = op->getAttr("functionized")) {
-                if (const auto boolAttr = dyn_cast<BoolAttr>(attr); boolAttr.getValue()) {
-                    opsToIsolate.push_back(op.getOperation());
-                }
-            }
-        });
-
-        // Once we marked all the ops, we isolate them
-        for (Operation *opToIsolate : opsToIsolate) {
-            isolateOp(opToIsolate, rewriter);
+        // Insert a new NopTOp at the end of the middle block if needed
+        if (!middleBlock->mightHaveTerminator()) {
+            rewriter.setInsertionPoint(middleBlock, middleBlock->end());
+            rewriter.create<qoalahost::NopTOp>(nextToOpToIsolate->getLoc());
         }
+
+        // We do not need to add a terminator op on the bottomBlock, since all the
+        // lower operations that were in the original block (including the original
+        // terminator) will end up in bottomBlock
     }
 }
