4129ce2da1fb1e6de8d0e00196b88c75d6e10470
spoukke <sacha.bernheim@hey.com>
2025-04-03 13:31:46 +0200

    factorize add deps pass in helper function

--- include/Analysis/QoalaHost/Helpers.h
+++ include/Analysis/QoalaHost/Helpers.h
@@ -4,59 +4,76 @@
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/IR/BuiltinOps.h"
 
-namespace qoala::analysis::isolate {
-    mlir::Operation *getNextOperation(mlir::Operation *op);
+namespace qoala::analysis {
+namespace isolate {
+mlir::Operation *getNextOperation(mlir::Operation *op);
+/**
+ * Isolate the given operation in its own block.
+ * @param opToIsolate Operation to isolate in its own block.
+ * @param rewriter The convertion patter rewriter to create new block
+ * terminators as needed.
+ */
+void isolateOp(mlir::Operation *opToIsolate,
+               mlir::ConversionPatternRewriter &rewriter);
 
-    /**
-     * Isolate the given operation in its own block.
-     * @param opToIsolate Operation to isolate in its own block.
-     * @param rewriter The convertion patter rewriter to create new block terminators as needed.
-     */
-    void isolateOp(mlir::Operation *opToIsolate, mlir::ConversionPatternRewriter &rewriter);
+using OpCheck = bool (*)(mlir::Operation *);
 
-    using OpCheck = bool (*)(mlir::Operation *);
+/**
+ * Isolate all the nested operations of a given type. The search will start from
+ * a given base operation and isolate all the found operations into a single
+ * block. Since creating new isolated blocks might create new blocks with no
+ * terminator operations, this method will also create new terminator operations
+ * as needed. This function also allows passing an extra check function to
+ * exclude the isolation of certain nested operations despite they match the
+ * types.
+ * @tparam FuncOpTy Type of the base function to start exploring for nested
+ * operations. This type should usually be "qoalahost::MainFuncOp", but can
+ * easily be other type with a nested region.
+ * @tparam OpTy Types of the operations to isolate.
+ * @param baseOp The base operation to explore for functions to isolate.
+ * @param rewriter A ConversionPatternRewriter object to create new block
+ * terminators as needed
+ * @param extraCheck An optional extra check function. If this given check
+ * returns false, the involved operation will not be marked to isolate, even if
+ * it is of the good type.
+ */
+template <typename FuncOpTy, typename... OpTy>
+void isolateOpsInNewBlocks(
+    FuncOpTy &baseOp, mlir::ConversionPatternRewriter &rewriter,
+    const std::optional<OpCheck> extraCheck = std::nullopt) {
+    auto mainFunction = dyn_cast<FuncOpTy>(&baseOp);
+    assert(mainFunction && "Trying to isolate the operations on an operation "
+                           "that is not a qoalahost.main_func");
+    mlir::SmallVector<mlir::Operation *> opsToIsolate;
 
-    /**
-     * Isolate all the nested operations of a given type. The search will start from a given base
-     * operation and isolate all the found operations into a single block. Since creating new
-     * isolated blocks might create new blocks with no terminator operations, this method
-     * will also create new terminator operations as needed.
-     * This function also allows passing an extra check function to exclude the isolation of
-     * certain nested operations despite they match the types.
-     * @tparam FuncOpTy Type of the base function to start exploring for nested operations. This
-     *                  type should usually be "qoalahost::MainFuncOp", but can easily be other
-     *                  type with a nested region.
-     * @tparam OpTy Types of the operations to isolate.
-     * @param baseOp The base operation to explore for functions to isolate.
-     * @param rewriter A ConversionPatternRewriter object to create new block terminators as needed
-     * @param extraCheck An optional extra check function. If this given check returns false, the
-     *                   involved operation will not be marked to isolate, even if it is of the
-     *                   good type.
-     */
-    template <typename FuncOpTy, typename... OpTy>
-    void isolateOpsInNewBlocks(FuncOpTy &baseOp, mlir::ConversionPatternRewriter &rewriter,
-        const std::optional<OpCheck> extraCheck = std::nullopt) {
-        auto mainFunction = dyn_cast<FuncOpTy>(&baseOp);
-        assert (mainFunction && "Trying to isolate the operations on an operation that is not a qoalahost.main_func");
-        mlir::SmallVector<mlir::Operation *> opsToIsolate;
-
-        // To correctly isolate the op, we will simply "mark them", since modifying
-        // the structure of the blocks will invalidate the internal iterators
-        // of the walk functions.
-        mainFunction->walk([&](mlir::Operation *op) {
-            if (isa<OpTy...>(op)) {
-                if (extraCheck.has_value() && !extraCheck.value()(op)) {
-                    return;
-                }
-                opsToIsolate.push_back(op);
+    // To correctly isolate the op, we will simply "mark them", since modifying
+    // the structure of the blocks will invalidate the internal iterators
+    // of the walk functions.
+    mainFunction->walk([&](mlir::Operation *op) {
+        if (isa<OpTy...>(op)) {
+            if (extraCheck.has_value() && !extraCheck.value()(op)) {
+                return;
             }
-        });
-
-        // Once we marked all the ops, we isolate them
-        for (mlir::Operation *opToIsolate : opsToIsolate) {
-            isolateOp(opToIsolate, rewriter);
+            opsToIsolate.push_back(op);
         }
+    });
+
+    // Once we marked all the ops, we isolate them
+    for (mlir::Operation *opToIsolate : opsToIsolate) {
+        isolateOp(opToIsolate, rewriter);
     }
 }
+} // namespace isolate
 
-#endif //HELPERS_H
+namespace dependencies {
+/**
+ * Track data, communication and NetQASM request dependencies between blocks.
+ * Make the dependencies information available by inserting a "nop_meta"
+ * operation At the beginning of each block.
+ * @param module module to walk for tracking and adding dependencies.
+ */
+void addDependencies(mlir::ModuleOp &module);
+} // namespace dependencies
+} // namespace qoala::analysis
+
+#endif // HELPERS_H
--- lib/Analysis/QoalaHost/AddDependencies.cpp
+++ lib/Analysis/QoalaHost/AddDependencies.cpp
@@ -1,9 +1,5 @@
-#include <vector>
-#include <unordered_set>
-
+#include "Analysis/QoalaHost/Helpers.h"
 #include "Dialect/QoalaHost/Passes.h"
-#include "Dialect/QoalaHost/QoalaHost.h"
-#include "Dialect/NetQASM/NetQASM.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Diagnostics.h"
 #include "llvm/Support/Debug.h"
@@ -11,7 +7,7 @@
 #define DEBUG_TYPE "qoalahost-add-deps-pass"
 
 using namespace mlir;
-using namespace qoala::dialects;
+using namespace qoala::analysis;
 
 namespace qoala::analysis {
 #define GEN_PASS_DEF_QOALAHOSTADDBLOCKDEPENDENCIES
@@ -25,133 +21,9 @@ class QoalaHostAddBlockDependenciesPass
 };
 
 void QoalaHostAddBlockDependenciesPass::runOnOperation() {
-    LLVM_DEBUG(llvm::dbgs() << "\n=== QoalaHostAddBlockDependenciesPass: "
-                               "Building Block Dependency Graph ===\n");
-
-    Operation *operation = getOperation();
-
-    // Block-level dependency graph: block -> set of blocks it depends on.
-    llvm::DenseMap<Block *, std::unordered_set<Block *>> blockDeps;
-
-    // Block -> string ID (e.g., block_0)
-    llvm::DenseMap<Block *, std::string> blockIdMap;
-    int idCounter = 0;
-
-    operation->walk([&](qoalahost::MainFuncOp mainFunc) {
-        for (Block &block : mainFunc.getBody().getBlocks()) {
-            std::string id = "block_" + std::to_string(idCounter++);
-            blockIdMap[&block] = id;
-        }
-
-        std::vector<Operation *> commOps;
-        std::vector<Operation *> requestCallOps;
-
-        mainFunc.walk([&](Operation *op) {
-            Block *consumerBlock = op->getBlock();
-
-            // Track classical communication ops
-            if (isa<qoalahost::SendIntsOp, qoalahost::RecvIntsOp,
-                    qoalahost::SendFloatsOp, qoalahost::RecvFloatsOp>(op)) {
-                commOps.push_back(op);
-            }
-
-            // Track request routine call ops
-            if (auto callOp = dyn_cast<qoalahost::CallOp>(op)) {
-                auto symRef = callOp.getCalleeAttr().dyn_cast<SymbolRefAttr>();
-                if (symRef) {
-                    Operation *callee =
-                        SymbolTable::lookupNearestSymbolFrom(operation, symRef);
-                    if (callee && isa<netqasm::RequestRoutineOp>(callee)) {
-                        requestCallOps.push_back(op);
-                    }
-                }
-            }
-
-            // Track data dependencies
-            for (Value operand : op->getOperands()) {
-                if (Operation *producer = operand.getDefiningOp()) {
-                    Block *producerBlock = producer->getBlock();
-                    if (producerBlock != consumerBlock &&
-                        blockDeps[consumerBlock].insert(producerBlock).second) {
-                        LLVM_DEBUG(llvm::dbgs()
-                                   << blockIdMap[consumerBlock] << " \n");
-                        consumerBlock->print(llvm::dbgs());
-                        LLVM_DEBUG(llvm::dbgs()
-                                   << "depends on " << blockIdMap[producerBlock]
-                                   << ":\n");
-                        producerBlock->print(llvm::dbgs());
-                        LLVM_DEBUG(llvm::dbgs() << "\n");
-                    }
-                }
-            }
-        });
-
-        // Classical communication ordering dependencies
-        LLVM_DEBUG(llvm::dbgs()
-                   << "\n=== Tracking communication dependencies ===\n");
-        for (size_t i = 1; i < commOps.size(); ++i) {
-            Block *prevBlock = commOps[i - 1]->getBlock();
-            Block *currBlock = commOps[i]->getBlock();
-
-            if (prevBlock != currBlock &&
-                blockDeps[currBlock].insert(prevBlock).second) {
-                LLVM_DEBUG(llvm::dbgs() << blockIdMap[currBlock] << " \n");
-                currBlock->print(llvm::dbgs());
-                LLVM_DEBUG(llvm::dbgs()
-                           << "depends on " << blockIdMap[prevBlock] << ":\n");
-                prevBlock->print(llvm::dbgs());
-                LLVM_DEBUG(llvm::dbgs() << "\n");
-            }
-        }
-
-        // Request routine call ordering dependencies
-        LLVM_DEBUG(llvm::dbgs()
-                   << "\n=== Tracking request routines dependencies ===\n");
-        for (size_t i = 1; i < requestCallOps.size(); ++i) {
-            Block *prevBlock = requestCallOps[i - 1]->getBlock();
-            Block *currBlock = requestCallOps[i]->getBlock();
-
-            if (prevBlock != currBlock &&
-                blockDeps[currBlock].insert(prevBlock).second) {
-                LLVM_DEBUG(llvm::dbgs() << blockIdMap[currBlock] << " \n");
-                currBlock->print(llvm::dbgs());
-                LLVM_DEBUG(llvm::dbgs()
-                           << "depends on " << blockIdMap[prevBlock] << ":\n");
-                prevBlock->print(llvm::dbgs());
-                LLVM_DEBUG(llvm::dbgs() << "\n");
-            }
-        }
-
-        // Insert NopMetaOp into each block with its ID and predecessor IDs
-        OpBuilder builder(mainFunc.getContext());
-
-        for (Block &block : mainFunc.getBody().getBlocks()) {
-            builder.setInsertionPointToStart(&block);
-
-            std::string blockId = blockIdMap[&block];
-            auto blockIdAttr = builder.getStringAttr(blockId);
-
-            // Collect and sort (for determinism) predecessor block IDs
-            std::vector<std::string> predIds;
-            for (Block *pred : blockDeps[&block]) {
-                predIds.push_back(blockIdMap[pred]);
-            }
-            std::sort(predIds.begin(), predIds.end());
-
-            std::vector<Attribute> predIdAttrs;
-            for (const auto &id : predIds) {
-                predIdAttrs.push_back(builder.getStringAttr(id));
-            }
-
-            auto predListAttr = builder.getArrayAttr(predIdAttrs);
-
-            builder.create<qoalahost::NopMetaOp>(block.front().getLoc(),
-                                                 blockIdAttr, predListAttr);
-
-            LLVM_DEBUG(llvm::dbgs()
-                       << "Inserted NopMetaOp in " << blockId
-                       << " with dependencies " << predListAttr << "\n");
-        }
-    });
+    ModuleOp module = dyn_cast<ModuleOp>(getOperation());
+    assert(module); // We expect the cast to succeed
+    LLVM_DEBUG(llvm::dbgs() << "QoalaHostAddBlockDependenciesPass\n");
+    dependencies::addDependencies(module);
 }
 } // namespace qoala::analysis
