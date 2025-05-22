f255f116e1d620edf5d73d8173b5955b1e3cc889
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2025-05-22 11:42:06 +0200

    Correctly set HIR to MIR target dialects, and apply a full dialect convertion

--- lib/Conversion/QoalaHIRToQoalaMIR/QoalaHIRToQoalaMIR.cpp
+++ lib/Conversion/QoalaHIRToQoalaMIR/QoalaHIRToQoalaMIR.cpp
@@ -1,3 +1,6 @@
+#include "mlir/IR/BuiltinDialect.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "llvm/Support/Debug.h"
@@ -28,11 +31,16 @@ namespace qoala::conversion {
         ConversionTarget target(context);
         // We add the legal dialects that we aim to keep in the target
         target.addLegalDialect<qmem::QMemDialect>();
+        target.addLegalDialect<BuiltinDialect>();
+        target.addLegalDialect<arith::ArithDialect>();
+        target.addLegalDialect<tensor::TensorDialect>();
         // We define the QNet dialect as "illegal", so the conversion will fail
         // if there are any qnet operations in the converted IR
         target.addIllegalDialect<qnet::QNetDialect>();
         // We also declare operations (classes) that can be declared legal in the target
-        // dialect. The `callback` argument (which receives the operation involved)
+        // dialect.
+        // We can also use the `addDynamicallyLegalOp` method to determine at runtime if
+        // an operation is legal. The `callback` argument (which receives the operation involved)
         // can determine if it is legal to leave the operation or not.
         /*
         target.addLegalOp<
@@ -67,11 +75,10 @@ namespace qoala::conversion {
                 hir::MeasureLowering
         >(typeConverter, &context);
 
-        // We finally apply a **partial** conversion, since there will be some
-        // operations that will stay... momentarily
-        LogicalResult result =
-            mlir::applyPartialConversion(operation, target, std::move(patterns));
-        if (mlir::failed(result)) {
+        // We finally apply a **full** conversion, since we correctly defined all the
+        // dialects that are "legal" in the target IR
+        LogicalResult result = applyFullConversion(operation, target, std::move(patterns));
+        if (failed(result)) {
             LLVM_DEBUG(llvm::dbgs() << *operation << "\n");
             signalPassFailure();
         }
