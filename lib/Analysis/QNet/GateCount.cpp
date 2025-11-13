88eb8d5a86b24f3cd3ca51e4589b9fc8404e2130
dferrari <dferrari@tudelft.nl>
2025-11-13 14:38:03 +0100

    remove dynamic casting

--- lib/Analysis/QNet/GateCount.cpp
+++ lib/Analysis/QNet/GateCount.cpp
@@ -34,56 +34,54 @@ namespace qoala::analysis::gatecount {
         uint32_t qId = 0;
 
         // Walk over all ops in the module.
-        module.walk([&](Operation *op) {
-            if (auto qubitOp = llvm::dyn_cast<QubitOpIface>(op)) {
-                if (llvm::isa<NewQubitOp, EprsOp>(qubitOp)) {
-                    for (Value result : qubitOp.getQubitResults()) {
-                        opResToId[result] = qId;
-                        LLVM_DEBUG(llvm::dbgs() << "New Qubit Op " << qubitOp.getName().getStringRef()
-                                                << " for qubit: " << qId << ".\n");
-                        detailedOneQubitGateCount[qId] = 0;
-                        detailedTwoQubitGateCount[qId] = 0;
-                        detailedGateCount[qId] = 0;
-                        ++qId;
-                    }
+        module.walk([&](QubitOpIface qubitOp) {
+            if (llvm::isa<NewQubitOp, EprsOp>(qubitOp)) {
+                for (Value result : qubitOp.getQubitResults()) {
+                    opResToId[result] = qId;
+                    LLVM_DEBUG(llvm::dbgs() << "New Qubit Op " << qubitOp.getName().getStringRef()
+                                            << " for qubit: " << qId << ".\n");
+                    detailedOneQubitGateCount[qId] = 0;
+                    detailedTwoQubitGateCount[qId] = 0;
+                    detailedGateCount[qId] = 0;
+                    ++qId;
+                }
+            } else {
+                ++gateCount;
+                if (isTwoQubitOp(qubitOp)) {
+                    LLVM_DEBUG(llvm::dbgs() << "Two Qubit Op: " << qubitOp.getName().getStringRef() << ".\n");
+                    ++twoQubitGateCount;
                 } else {
-                    ++gateCount;
-                    if (isTwoQubitOp(qubitOp)) {
-                        LLVM_DEBUG(llvm::dbgs() << "Two Qubit Op: " << qubitOp.getName().getStringRef() << ".\n");
-                        ++twoQubitGateCount;
-                    } else {
-                        LLVM_DEBUG(llvm::dbgs() << "One Qubit Op: " << qubitOp.getName().getStringRef() << ".\n");
-                        ++oneQubitGateCount;
-                    }
-                    // For each operand, find its init Id and propagate it to the corresponding result.
-                    // As per qnet definition of quantum ops, #results <= #operands and qubits are always the first
-                    // operands. Also order of operands and results should be the same, for SSA purposes. Loop through
-                    // operands, and for each operand that we are already tracking, i.e. a qubit, find its corresponding
-                    // results at the same position.
-                    for (auto operandIt : llvm::enumerate(qubitOp.getQubitOperands())) {
-                        Value operand = operandIt.value();
-                        // Qubit operand index
-                        uint32_t index = operandIt.index();
+                    LLVM_DEBUG(llvm::dbgs() << "One Qubit Op: " << qubitOp.getName().getStringRef() << ".\n");
+                    ++oneQubitGateCount;
+                }
+                // For each operand, find its init Id and propagate it to the corresponding result.
+                // As per qnet definition of quantum ops, #results <= #operands and qubits are always the first
+                // operands. Also order of operands and results should be the same, for SSA purposes. Loop through
+                // operands, and for each operand that we are already tracking, i.e. a qubit, find its corresponding
+                // results at the same position.
+                for (auto operandIt : llvm::enumerate(qubitOp.getQubitOperands())) {
+                    Value operand = operandIt.value();
+                    // Qubit operand index
+                    uint32_t index = operandIt.index();
 
-                        // Check if the operand is a qubit we are tracking
-                        if (opResToId.contains(operand)) {
-                            uint32_t initId = opResToId.at(operand);
+                    // Check if the operand is a qubit we are tracking
+                    if (opResToId.contains(operand)) {
+                        uint32_t initId = opResToId.at(operand);
 
-                            // Add this operation to the history of the qubit it acts on.
-                            LLVM_DEBUG(llvm::dbgs() << "Op " << qubitOp.getName().getStringRef()
-                                                    << " on qubit: " << initId << ".\n");
-                            detailedGateCount[initId]++;
-                            if (isTwoQubitOp(qubitOp)) {
-                                detailedTwoQubitGateCount[initId]++;
-                            } else {
-                                detailedOneQubitGateCount[initId]++;
-                            }
-
-                            // Propagate the init Id to the corresponding result.
-                            // Resut index should be same as corresponding operand.
-                            Value result = qubitOp.getQubitResult(index);
-                            opResToId[result] = initId;
+                        // Add this operation to the history of the qubit it acts on.
+                        LLVM_DEBUG(llvm::dbgs() << "Op " << qubitOp.getName().getStringRef()
+                                                << " on qubit: " << initId << ".\n");
+                        detailedGateCount[initId]++;
+                        if (isTwoQubitOp(qubitOp)) {
+                            detailedTwoQubitGateCount[initId]++;
+                        } else {
+                            detailedOneQubitGateCount[initId]++;
                         }
+
+                        // Propagate the init Id to the corresponding result.
+                        // Resut index should be same as corresponding operand.
+                        Value result = qubitOp.getQubitResult(index);
+                        opResToId[result] = initId;
                     }
                 }
             }
