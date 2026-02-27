ab2a93e35f88c302dd1b333f7a618efceb56ad34
dferrari <dferrari@tudelft.nl>
2026-02-27 15:07:55 +0100

    add branching support to qmem efficiency

--- lib/Analysis/QoalaHost/QMemoryEfficiency.cpp
+++ lib/Analysis/QoalaHost/QMemoryEfficiency.cpp
@@ -1,9 +1,11 @@
 #include "Analysis/QoalaHost/Helpers.h"
 #include "Dialect/NetQASM/NetQASM.h"
 #include "Dialect/QoalaHost/QoalaHost.h"
+#include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 #include "mlir/IR/Operation.h"
 
 #define DEBUG_TYPE "qoalahost-qmemory-efficiency"
@@ -13,53 +15,117 @@ using namespace qoala::dialects;
 
 namespace qoala::analysis::qmemeff {
 
+    /// Process qalloc and measure operations inside a callee function,
+    /// updating virtualQubits, physicalQubits and measured buffer accordingly.
+    static void processCalleeQMemOps(FunctionOpInterface callee, uint32_t &virtualQubits, uint32_t &physicalQubits,
+                                     uint32_t &measured) {
+        callee.walk([&](Operation *opInCallee) {
+            llvm::TypeSwitch<Operation *>(opInCallee)
+                    .Case([&](const netqasm::QAllocOp qallocOp) {
+                        LLVM_DEBUG(llvm::dbgs() << "Found QAllocOp: " << qallocOp << "\n");
+                        ++virtualQubits;
+                        if (measured == 0) {
+                            ++physicalQubits;
+                        } else {
+                            --measured;
+                        }
+                    })
+                    .Case([&](const netqasm::ifaces::MeasOp measureOp) {
+                        LLVM_DEBUG(llvm::dbgs() << "Found MeasureOp: " << measureOp << "\n");
+                        ++measured;
+                    });
+        });
+    }
+
+    // State to track during branch analysis.
+    struct BranchCounts {
+        uint32_t virtualQubits;
+        uint32_t physicalQubits;
+        uint32_t measured;
+
+        void dumpInto(uint32_t &virt, uint32_t &phys, uint32_t &meas) const {
+            virt = virtualQubits;
+            phys = physicalQubits;
+            meas = measured;
+        }
+    };
+
+    /**
+     * Traverse CFG and count qalloc/measure ops.
+     * For conditional branches, evaluates both paths and selects the one with
+     * the worst memory efficiency (highest physicalQubits).
+     */
+    static void traverseCFGAndCountQMem(mlir::Block *block, llvm::DenseSet<mlir::Block *> &visited,
+                                        uint32_t &virtualQubits, uint32_t &physicalQubits, uint32_t &measured) {
+        if (visited.contains(block))
+            return;
+        visited.insert(block);
+
+        LLVM_DEBUG(llvm::dbgs() << "Visiting block.\n");
+
+        // Process all operations in current block except CF terminators.
+        for (auto &op : block->getOperations()) {
+            if (isa<cf::CondBranchOp, cf::BranchOp>(op))
+                break;
+
+            if (auto callOp = dyn_cast<qoalahost::CallOp>(&op)) {
+                auto callee = callOp.getCalleeOperation<FunctionOpInterface>();
+                processCalleeQMemOps(callee, virtualQubits, physicalQubits, measured);
+            }
+        }
+
+        // Handle branching via terminator.
+        auto terminator = block->getTerminator();
+        if (auto condBr = dyn_cast<cf::CondBranchOp>(terminator)) {
+            LLVM_DEBUG(llvm::dbgs() << "Evaluating conditional branch.\n");
+            // Capture initial state
+            BranchCounts initialState{virtualQubits, physicalQubits, measured};
+
+            // Analyze true branch
+            llvm::DenseSet<Block *> visitedTrue = visited;
+            traverseCFGAndCountQMem(condBr.getTrueDest(), visitedTrue, virtualQubits, physicalQubits, measured);
+            BranchCounts trueCounts{virtualQubits, physicalQubits, measured};
+            LLVM_DEBUG(llvm::dbgs() << "TRUE branch evaluated: physicalQubits=" << trueCounts.physicalQubits << ".\n");
+
+            // Restore initial state and analyze false branch
+            initialState.dumpInto(virtualQubits, physicalQubits, measured);
+            llvm::DenseSet<Block *> visitedFalse = visited;
+            traverseCFGAndCountQMem(condBr.getFalseDest(), visitedFalse, virtualQubits, physicalQubits, measured);
+            BranchCounts falseCounts{virtualQubits, physicalQubits, measured};
+            LLVM_DEBUG(llvm::dbgs() << "FALSE branch evaluated: physicalQubits=" << falseCounts.physicalQubits << ".\n");
+
+            // Pick worst case: highest physicalQubits means worst efficiency.
+            if (trueCounts.physicalQubits >= falseCounts.physicalQubits) {
+                trueCounts.dumpInto(virtualQubits, physicalQubits, measured);
+                visited = visitedTrue;
+            } else {
+                // Variables already contain false branch state
+                visited = visitedFalse;
+            }
+        } else if (auto br = dyn_cast<cf::BranchOp>(terminator)) {
+            traverseCFGAndCountQMem(br.getDest(), visited, virtualQubits, physicalQubits, measured);
+        } else if (auto *nextBlock = block->getNextNode()) {
+            traverseCFGAndCountQMem(nextBlock, visited, virtualQubits, physicalQubits, measured);
+        }
+    }
+
     QoalaHostQMemoryEfficiency::QoalaHostQMemoryEfficiency(Operation *op) {
-        // Compute quantum memory efficiency as 1-(virtualQubits/physicalQubits), the closer to 1 the better.
+        // Compute quantum memory efficiency as 1-(physicalQubits/virtualQubits), the closer to 1 the better.
         // Virtual qubits correspond to the number of qalloc ops present in a program,
         // physical qubits instead refers to the actual qubits that will be used by
-        // the QNPU during program execution. This two numbers may differ as each time
+        // the QNPU during program execution. These two numbers may differ as each time
         // a qubit is measured, its memory location is freed and can be reused by a subsequent qalloc op.
         // By tracking the order of qalloc and measurement ops, one can compute how many physical qubits
         // will be active at most at the same time during a program execution.
-        // Some programs may have efficiency 0, for example a program
-        // where only one qubit is allocated and measured.
-        // Locate main function
+        // For conditional branches, both paths are evaluated and the worst case
+        // (highest physical qubit count) is selected.
+
         auto mainFunc = dyn_cast_or_null<qoalahost::MainFuncOp>(*op);
         assert(mainFunc && "No main func? This is embarrassing...");
 
-        // Measurements ops will increase a "buffer" of memory positions to be reused.
         uint32_t measured = 0;
-
-        // Walk through the blocks in the main function and focus on qoalahost call ops,
-        // then walk through each operation in the callee and search for qalloc and measure operations.
-
-        // TODO Current implementation does not take into account epr request callbacks.
-        // In the future, add support for epr request callbacks if needed. See issue qoala-kanban-board#89.
-        const auto callOps = mainFunc.getOps<qoalahost::CallOp>();
-
-        for (auto callOp : callOps) {
-            auto callee = callOp.getCalleeOperation<FunctionOpInterface>();
-            callee.walk([&](Operation *opInCallee) {
-                llvm::TypeSwitch<Operation *>(opInCallee)
-                        .Case([&](const netqasm::QAllocOp qallocOp) {
-                            LLVM_DEBUG(llvm::dbgs() << "Found QAllocOp: " << qallocOp << "\n");
-                            // Increase virtual qubits for every qalloc op.
-                            ++virtualQubits;
-                            // Increase physical qubits only if the measured buffer equals 0.
-                            if (measured == 0) {
-                                ++physicalQubits;
-                            } else {
-                                // Remember to decrease the measure buffer.
-                                --measured;
-                            }
-                        })
-                        .Case([&](const netqasm::ifaces::MeasOp measureOp) {
-                            LLVM_DEBUG(llvm::dbgs() << "Found MeasureOp: " << measureOp << "\n");
-                            // Each measure op will increase the measured buffer.
-                            ++measured;
-                        });
-            });
-        }
+        llvm::DenseSet<Block *> visited;
+        traverseCFGAndCountQMem(&mainFunc.front(), visited, virtualQubits, physicalQubits, measured);
     }
 
     float QoalaHostQMemoryEfficiency::getEfficiency() const {
---
+++ test/Passes/QMemoryEfficiency/multi-branch.mlir
@@ -0,0 +1,124 @@
+// RUN: qoala-opt %s --qoalahost-show-analysis-qmem-eff | FileCheck %s
+// CHECK: [QMem Efficiency]:2.500000e-01
+
+module {
+  qremote.remote @Bob
+  netqasm.local_routine private @__qoala_convert_float_angle(f32) -> (i32, i32)
+  netqasm.local_routine @__qoala_wrapper0() -> i32 {
+    %0 = netqasm.qalloc  : i32
+    netqasm.init %0
+    netqasm.return %0 : i32
+  }
+  netqasm.local_routine @__qoala_wrapper1() -> i32 {
+    %0 = netqasm.qalloc  : i32
+    netqasm.init %0
+    netqasm.return %0 : i32
+  }
+  netqasm.local_routine @__qoala_wrapper2(%arg0: i32) -> i1 {
+    %0 = netqasm.measure %arg0 : i1
+    netqasm.return %0 : i1
+  }
+  netqasm.local_routine @__qoala_wrapper3() {
+    %0 = netqasm.qalloc  : i32
+    netqasm.init %0
+    netqasm.return
+  }
+  netqasm.local_routine @__qoala_wrapper4(%arg0: i32) -> i1 {
+    %0 = netqasm.measure %arg0 : i1
+    netqasm.return %0 : i1
+  }
+  netqasm.request_routine @__qoala_wrapper5() -> i32 {
+    %0 = netqasm.qalloc  : i32
+    netqasm.eprs %0 {remote = @Bob}
+    netqasm.return %0 : i32
+  }
+  netqasm.local_routine @__qoala_wrapper6(%arg0: i32) {
+    netqasm.x %arg0
+    netqasm.return
+  }
+  netqasm.local_routine @__qoala_wrapper7(%arg0: i32) {
+    netqasm.z %arg0
+    netqasm.return
+  }
+  netqasm.local_routine @__qoala_wrapper8(%arg0: i32) {
+    netqasm.y %arg0
+    netqasm.return
+  }
+  netqasm.local_routine @__qoala_wrapper9(%arg0: i32) -> i1 {
+    %0 = netqasm.measure %arg0 : i1
+    netqasm.return %0 : i1
+  }
+  qoalahost.main_func @qmem_eff() {
+    qoalahost.blk_meta  {block_id = "block_0", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    qoalahost.remote_id_ref  {classical = true, quantum = true, remote = @Bob}
+    qoalahost.nop_term
+  ^bb1:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_1", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    %c1_i32 = arith.constant 1 : i32
+    %true = arith.constant true
+    qoalahost.nop_term
+  ^bb2:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_2", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    %0 = qoalahost.call @__qoala_wrapper0() : () -> i32
+  ^bb3:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_3", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    %1 = qoalahost.call @__qoala_wrapper1() : () -> i32
+  ^bb4:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_4", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    qoalahost.nop_term
+  ^bb5:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_5", deadlines = {}, dependencies = ["block_0"], predecessors = [], prev_comm = "", prev_ent = ""}
+    %2 = qoalahost.recv_int  {remote = @Bob} : i32
+  ^bb6:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_6", deadlines = {}, dependencies = ["block_1", "block_2", "block_3", "block_5"], predecessors = [], prev_comm = "", prev_ent = ""}
+    %4 = arith.cmpi eq, %2, %c1_i32 : i32
+    cf.cond_br %4, ^bb7, ^bb9
+  ^bb7:  // pred: ^bb7
+    qoalahost.blk_meta  {block_id = "block_7", deadlines = {}, dependencies = ["block_3"], predecessors = ["block_6"], prev_comm = "", prev_ent = ""}
+    %5 = qoalahost.call @__qoala_wrapper2(%1) : (i32) -> i1
+  ^bb8:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_8", deadlines = {}, dependencies = ["block_7"], predecessors = ["block_7"], prev_comm = "", prev_ent = ""}
+    cf.br ^bb10(%5 : i1)
+  ^bb9:  // pred: ^bb7
+    qoalahost.blk_meta  {block_id = "block_9", deadlines = {}, dependencies = ["block_1"], predecessors = ["block_6"], prev_comm = "", prev_ent = ""}
+    cf.br ^bb10(%true : i1)
+  ^bb10(%6: i1):  // 2 preds: ^bb9, ^bb10
+    qoalahost.blk_meta  {block_id = "block_10", deadlines = {}, dependencies = [], predecessors = ["block_8", "block_9"], prev_comm = "", prev_ent = ""}
+    cf.br ^bb11
+  ^bb11:  // pred: ^bb11
+    qoalahost.blk_meta  {block_id = "block_11", deadlines = {}, dependencies = [], predecessors = ["block_10"], prev_comm = "", prev_ent = ""}
+    qoalahost.call @__qoala_wrapper3() : () -> ()
+  ^bb12:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_12", deadlines = {}, dependencies = ["block_2"], predecessors = [], prev_comm = "", prev_ent = ""}
+    %7 = qoalahost.call @__qoala_wrapper4(%0) : (i32) -> i1
+  ^bb13:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_13", deadlines = {}, dependencies = [], predecessors = [], prev_comm = "", prev_ent = ""}
+    %3 = qoalahost.call @__qoala_wrapper5() : () -> i32
+  ^bb14:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_14", deadlines = {}, dependencies = ["block_1", "block_13"], predecessors = [], prev_comm = "", prev_ent = ""}
+    %8 = arith.extui %6 : i1 to i32
+    %9 = arith.cmpi eq, %8, %c1_i32 : i32
+    cf.cond_br %9, ^bb15, ^bb18
+  ^bb15:  // pred: ^bb14
+    qoalahost.blk_meta  {block_id = "block_15", deadlines = {}, dependencies = ["block_13"], predecessors = ["block_14"], prev_comm = "", prev_ent = ""}
+    qoalahost.call @__qoala_wrapper6(%3) : (i32) -> ()
+  ^bb16:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_16", deadlines = {}, dependencies = ["block_15"], predecessors = [], prev_comm = "", prev_ent = ""}
+    qoalahost.call @__qoala_wrapper7(%3) : (i32) -> ()
+  ^bb17:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_17", deadlines = {}, dependencies = ["block_16"], predecessors = ["block_16"], prev_comm = "", prev_ent = ""}
+    cf.br ^bb20
+  ^bb18:  // pred: ^bb14
+    qoalahost.blk_meta  {block_id = "block_18", deadlines = {}, dependencies = ["block_16"], predecessors = ["block_14"], prev_comm = "", prev_ent = ""}
+    qoalahost.call @__qoala_wrapper8(%3) : (i32) -> ()
+  ^bb19:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_19", deadlines = {}, dependencies = ["block_18"], predecessors = ["block_18"], prev_comm = "", prev_ent = ""}
+    cf.br ^bb20
+  ^bb20:  // 2 preds: ^bb17, ^bb19
+    qoalahost.blk_meta  {block_id = "block_20", deadlines = {}, dependencies = ["block_18"], predecessors = ["block_17", "block_19"], prev_comm = "", prev_ent = ""}
+    %10 = qoalahost.call @__qoala_wrapper9(%3) : (i32) -> i1
+  ^bb21:  // no predecessors
+    qoalahost.blk_meta  {block_id = "block_21", deadlines = {}, dependencies = ["block_20"], predecessors = [], prev_comm = "", prev_ent = ""}
+    qoalahost.return %10 : i1
+  }
+}
